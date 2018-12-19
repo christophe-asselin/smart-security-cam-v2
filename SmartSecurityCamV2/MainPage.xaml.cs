f497413f045c63af405a4a019dffbc0ec4526ce6
christophe-asselin <christophe.asselin@hotmail.com>
2018-12-18 23:23:20 -0500

    Implemented basic motion detection and created basic GUI

--- SmartSecurityCamV2/MainPage.xaml.cs
+++ SmartSecurityCamV2/MainPage.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Devices.Gpio;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -22,9 +23,37 @@ namespace SmartSecurityCamV2
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private const int SENSOR_PIN = 4;
+        private GpioPin sensorPin;
+
         public MainPage()
         {
             this.InitializeComponent();
+            InitGpio();
+            Unloaded += MainPageUnloaded;
+        }
+
+        private void InitGpio()
+        {
+            var gpio = GpioController.GetDefault();
+            sensorPin = gpio.OpenPin(SENSOR_PIN);   
+            sensorPin.SetDriveMode(GpioPinDriveMode.Input);
+            sensorPin.ValueChanged += SensorPinValueChanged;
+        }
+
+        private void SensorPinValueChanged(GpioPin sender, GpioPinValueChangedEventArgs arg)
+        {
+            bool isOn = (arg.Edge == GpioPinEdge.RisingEdge);
+
+            if (isOn)
+            {
+                // take picture
+            }
+        }
+
+        private void MainPageUnloaded(object sender, object arg)
+        {
+            sensorPin.Dispose();
         }
     }
 }
